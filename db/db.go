e33ec5f66f6fa5c300593fb9fa482b23fe65691c
Iede Snoek <info@esoxsolutions.nl>
2023-06-26 14:25:44 +0200

    First attempt at using go coroutines

--- db/db.go
+++ db/db.go
@@ -11,6 +11,11 @@ import (
 func InitializeDatabaseConnection() (*gorm.DB, error) {
 	dsn := ConstructDsn()
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in InitializeDatabaseConnection", r)
+		}
+	}()
 	//if err != nil {
 	//		panic("Failed to connect to database")
 	//}
@@ -29,7 +34,7 @@ func getEnvironmentVariableWithDefault(key string, defaultValue string) string {
 func ConstructDsn() string {
 	host := getEnvironmentVariableWithDefault("host", "localhost")
 	user := getEnvironmentVariableWithDefault("user", "postgres")
-	password := getEnvironmentVariableWithDefault("password", "<secret>")
+	password := getEnvironmentVariableWithDefault("password", "Piloten2030")
 	dbname := getEnvironmentVariableWithDefault("dbname", "webevents")
 	port := getEnvironmentVariableWithDefault("port", "5432")
 
--- operations/operations.go
+++ operations/operations.go
@@ -9,65 +9,92 @@ import (
 )
 
 func CreateEvent(c *gin.Context) {
+	result := make(chan *models.WebEvent)
 	var webEvent models.WebEvent
-	go func() {
+	db, err := db.GetConnection()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	}
+	go func(context *gin.Context) {
 		c.BindJSON(&webEvent)
-		db, err := db.GetConnection()
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, err)
+
+		dbResult := db.Connection.Create(&webEvent)
+		if dbResult.Error != nil {
+			result <- nil
+		} else {
+			result <- &webEvent
 		}
-		db.Connection.Create(&webEvent)
-		c.JSON(http.StatusCreated, webEvent)
-	}()
+
+	}(c.Copy())
+	finalResult := <-result
+	if finalResult == nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to create event"})
+	} else {
+		c.JSON(http.StatusOK, finalResult)
+	}
 }
 
 func ListEvents(c *gin.Context) {
 	result := make(chan []models.WebEvent)
+
+	db, err := db.GetConnection()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	}
+
 	go func(context *gin.Context) {
 		var webEvents []models.WebEvent
 
-		db, err := db.GetConnection()
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, err)
-		}
 		db.Connection.Find(&webEvents)
 		result <- webEvents
+
 	}(c.Copy())
 	c.JSON(http.StatusOK, <-result)
+
 }
 
 func FindEvent(c *gin.Context) {
+	result := make(chan *models.WebEvent)
 	var webEvent models.WebEvent
-	go func() {
+	db, err := db.GetConnection()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	}
+	go func(context *gin.Context) {
 		id := c.Param("id")
 
-		db, err := db.GetConnection()
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, err)
-		}
-		result := db.Connection.First(&webEvent, id)
-		if result.Error != nil {
-			c.JSON(http.StatusNotFound, nil)
+		dbResult := db.Connection.First(&webEvent, id)
+		if dbResult.Error != nil {
+			result <- nil
 		} else {
-			c.JSON(http.StatusOK, webEvent)
+			result <- &webEvent
 		}
-	}()
+	}(c.Copy())
+	var finalResult *models.WebEvent = <-result
+	if finalResult == nil {
+		c.JSON(http.StatusNotFound, gin.H{})
+	} else {
+		c.JSON(http.StatusOK, finalResult)
+	}
 }
 
 func DeleteEvent(c *gin.Context) {
-	go func() {
+	result := make(chan gin.H)
+	db, err := db.GetConnection()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	}
+	go func(context *gin.Context) {
 		id := c.Param("id")
-		db, err := db.GetConnection()
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, err)
-		}
-		result := db.Connection.Delete(&models.WebEvent{}, id)
-		if result.Error != nil {
-			c.JSON(http.StatusInternalServerError, nil)
+
+		dbResult := db.Connection.Delete(&models.WebEvent{}, id)
+		if dbResult.Error != nil {
+			result <- gin.H{"message": "Failed to delete event"}
 		} else {
-			c.JSON(http.StatusAccepted, gin.H{
+			result <- gin.H{
 				"message": "Deleted",
-			})
+			}
 		}
-	}()
+	}(c.Copy())
+	c.JSON(http.StatusOK, <-result)
 }
